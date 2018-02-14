b3233f9783eed8b874fedabb9ccf8ae3936b2193
Sagar <sagarmalik1805@gmail.com>
2018-02-14 19:04:30 +0530

    Node completed :)

--- Node.java
+++ Node.java
@@ -2,30 +2,24 @@ import java.util.*;
 
 public class Node{
     int id;
-    ArrayList<Double> receivedStamps;
+    ArrayList<Double> receivedStamps;				//to store transactions in file ?
     Random rn = new Random();
     ArrayList<Node> peers;     //pointers to Nodes are stored here
     boolean fast;
     double lambda;
-    boolean type;           // True => Fast CPU
     HashMap<Integer,Double> balances;
     HashMap<Block,HashSet<Block>> pendingBlock;
     HashMap<Block,HashSet<Block>> blocks;
     Block lastBlock;
-	ArrayList<Transaction> transactions;
+	HashSet<Transaction> transactions;
+	HashMap<Integer,Transaction> mapping;
 	int num;
-
+	HashMap<Block,boolean> inChain;
     Node(int id, boolean fast, double lambda, Block b,int n){
         this.id = id;
         this.fast = fast;
         this.lambda = lambda;
         this.balances=new HashMap<Integer,Double>();
-        if(lambda > 0.5 ){                                                              //////// check for lambda value
-            this.type = true;
-        }
-        else{
-            this.type = false;
-        }
         receivedStamps = new ArrayList<Double>();
         peers = new ArrayList<Node>();
         lastBlock=b;
@@ -33,7 +27,9 @@ public class Node{
         	balances.put(i,0.0);
         }
         this.num=n;
-    	ArrayList<Transaction> transactions=new ArrayList<Transaction>();
+        inChain=new HashMap<Block,boolean>();
+    	transactions=new HashSet<Transaction>();
+    	mapping=new HashMap<Integer,Transaction>();
     }
 
     public void setPeers(int n, ArrayList<Node> all){
@@ -44,51 +40,120 @@ public class Node{
                 peers.add(all.get(i));
         }
     }
-    public void checkPending(Block b){
-    	if (!pendingBlock.containsKey(b)){
-    		HashSet<Block> child=new HashSet<Block>();
-			blocks.put(b,child);
-    	}
-    	else{
-    		HashSet<Block> childs=pendingBlock.get(b);
-    		blocks.put(b,chils);
-    		for (Block child : )
+
+    public void checkPending(Block b){		
+    	for (Block c: pendingBlock.keySet()){
+    		if ((c.parent).equals(b)){
+    			blocks.get(b).add(c);
+    			HashSet<Block> child=new HashSet<Block>();
+    			blocks.put(c,child);
+    			pendingBlock.remove(c);
+    			findlongest(c);
+    			checkPending(c);
+    		}
     	}
     }
-    public void receiveBlock(Node n,Block b,Simulator s){
-    	Block parent=b.previous_block;
-    	if (!blocks.containsKey(b)){
-			if (lastBlock.length<b.length || ((lastBlock.length==b.length)&& (lastBlock.timestamp>b.timestamp)){
-				// this.balances=new HashMap<int,double>(n.balances);	//deep copy of balances
-				// this.lastBlock=n.lastBlock;
-				
-				if (blocks.containsKey(parent)){
-					HashSet<Block> childs=blocks.get(parent);
-					childs.add(b);
-					checkPending(b);
-					// HashSet<Block> child=new HashSet<Block>();
-					// blocks.put(b,child);
+    public void changeChain(Block b){
+    	Block b1=lastBlock;
+    	while (b1.id!=0){
+    		inChain.put(b1,false);
+    		b1=b1.previous_block;
+    	}
+    	b1=b;
+    	while (b1.id!=0){
+    		inChain.put(b1,true);
+    		b1=b1.previous_block;
+    	}
+    	b1=lastBlock;
+    	while (inChain.get(b1)==false){
+    		undo(b1);
+    		b1=b1.previous_block;
+    	}
+    	Block b2=b;
+    	while (b2.id!=b1.id){
+    		exec(b2);
+    		b2=b2.previous_block;
+    	}
+    	lastBlock=b;
+    }
+    public void exec(Block b){
+    	HashSet<Transaction> ts=b.transactions;
+    	for( Transaction t : ts){
+    		Transaction t1=mapping.get(t.tID);
+    		if (t1.spent==false){
+	    		t1.spent=true;
+	    		balances.put(t1.fromID,balances.get(t1.fromID)-t1.amount);
+	    		balances.put(t1.toID,balances.get(t1.toID)+t1.amount);
+    		}
+    	}
+    }
+    public void undo(Block b){
+    	HashSet<Transaction> ts=b.transactions;
+    	for( Transaction t : ts){
+    		Transaction t1=mapping.get(t.tID);
+    		if (t1.spent==true){
+	    		t1.spent=false;
+	    		balances.put(t1.fromID,balances.get(t1.fromID)+t1.amount);
+	    		balances.put(t1.toID,balances.get(t1.toID)-t1.amount);
+    		}
+    	}
+    }
 
-					// check for pending blocks lastBlock=b;
+    public void findLongest(Block b){
+    	inChain.put(b,false);
+    	if ((b.parent).equals(lastBlock)){
+    		lastBlock =b;
+    		inChain.put(b,true);
+    		exec(b);		//execute transactions in this block
+    	}
+    	else{
+    		if ((b.length>lastBlock.length)|| ((b.length==lastBlock.length) && (b.timestamp<lastBlock.timestamp))){
+    			changeChain(b);
+    		}
+    	}
+    }
+    public void receiveBlock(Block b,Simulator s){		//generate new block
+    	Block parent=b.previous_block;
+
+		if (blocks.containsKey(parent)){
+			HashSet<Block> childs=blocks.get(parent);
+			if (!childs.contains(b)){
+				childs.add(b);
+				HashSet<Block> child=new HashSet<Block>();
+				blocks.put(b,child);
+				Block b1=lastBlock;
+				findLongest(b);
+				// check if its child is in pending block
+				checkPending(b);
+
+				if (!b1.equals(lastBlock)){
+					System.out.println("received block "+ b.id +" changed last block from "+ b1.id +"to"+ lastBlock.id);
+					int id1=s.getNextBlockId();
+					generate(s,id1);
 				}
 				else{
-					if (!pendingBlock.containsKey(parent)){
-						HashSet<Block> child=new HashSet<Block>();
-						child.add(b);
-						pendingBlock.put(parent,child);
-					}
-					else{
-						pendingBlock.get(parent).add(b);
-					}
+					System.out.println("received block "+ b.id +" but still last block "+ lastBlock.id +" is same");
 				}
+			}
+		}
+		else{
+			if (pendingBlock.containsKey(parent)){
+				if (!pendingBlock.containsKey(b)){
+					HashSet<Block> child=new HashSet<Block>();
+					pendingBlock.put(b,child);
+					pendingBlock.get(parent).add(b);
+				}
+			}
+			if (!pendingBlock.containsKey(b)){
+				HashSet<Block> child=new HashSet<Block>();
+				pendingBlock.put(b,child);
 			}
 		}
 	}
 
 	public void receiveTransaction(Transaction t,Simulator s){
 		boolean check=false;
-		for (int i=0;i<transactions.size();i++){
-			Transaction t1=transactions.get(i);
+		for (Transaction t1: transactions){
 			if (t1.tID==t.tID){
 				check=true;
 				break;
@@ -97,20 +162,23 @@ public class Node{
 		if (!check){
 			Transaction t2=new Transaction(t);
 			transactions.add(t2);
+			mapping.put(t2.tID,t2);
 			broadcastTransaction(t2);
 		}
+		receivedStamps.add(s.curr_time);
 	}
 
 	public void broadcastTransaction(Transaction t,Simulator s){
 		double time=s.curr_time;
+		System.out.println("broadcasting transaction "+ t.tID +" from "+ this.id);
 		for (int i=0;i<peers.size();i++){
-			int id1=peers.get(i);
+			int id1=peers.get(i).id;
 			double latency=s.latency(this.id,id1,0);
 			Task task=new Task(3,curr_time+latency,id1);			//type,time,node
 			s.MainQueue.add(task);
 		}
 	}
-
+																							///////transactions ko mapping se replace krdio lodu
 	public void generateTransaction(Simulator s,int Tid){
 		double time=s.curr_time;
 		double c=Math.log(1-Math.random())/(-s.LAMBDA_TRANS);
@@ -123,45 +191,51 @@ public class Node{
 		while (money==0.0){
 			money=(rn.nextDouble())*balances.get(id);
 		}
-		Transaction t=new Transaction(Tid,this.id,i,money);
+		Transaction t=new Transaction(Tid,this.id,i,money,time);
 		transactions.add(t);
-
+		mapping.put(t.tID,t);
 		Task task=new Task(2,curr_time+c,this.id);		//type of generate next transaction,time,id
 		s.MainQueue.add(task);
-
 		broadcastTransaction(t,s);
+		System.out.println("generated Transaction "+ Tid.tID +" from "+ this.id +" to"+ i);
 	}
 
 	public void generateBlock(Simulator s,int id1,Block b){
+		if (lastBlock.id==b.id){
+			generate(s,id1);
+		}
+	}
+
+	public void generate(Simulator s,int id1){
+
 		double time=s.curr_time;
 		double c=Math.log(1-Math.random())/(-s.LAMBDA_TK);
-		if (lastBlock.id==b.id){
-			Block block=new Block(id1,time,lastBlock,this.id,lastBlock.length+1);
-			HashSet<Transaction> trans=new HashSet<Transaction>();
-			for (int i=0;i<transactions.size();i++){
-				transactions t=transactions.get(i);
-				
-				if (balances.get(t.fromID)>t.amount && t.spent==false){
+		Block block=new Block(id1,time,lastBlock,this.id,lastBlock.length+1);
+		HashSet<Transaction> trans=new HashSet<Transaction>();
+		for (Transaction t:transactions){		
+			if (balances.get(t.fromID)>=t.amount && t.spent==false){
 				trans.add(t);
 				balances.put(t.fromID,balances.get(t.fromID)-t.amount);
 				balances.put(t.toID,balances.get(t.toID)+t.amount);
 				t.spent=true;
 			}
-			}
-			block.transactions=trans;
-			lastBlock=block;
-			Task task=new Task(0,time+c,id1,block);
-			s.MainQueue.add(task);
-			broadcastBlock(s,block);
 		}
+		block.transactions=trans;
+		lastBlock=block;
+		inChain.put(lastBlock,true);
+		Task task=new Task(0,time+c,id1,lastBlock);
+		s.MainQueue.add(task);
+		System.out.println("generated Block "+ id1 +" by "+ this.id );
+		broadcastBlock(s,block);
 	}
 
 	public void broadcastBlock(Simulator s,Block b){
-
 		double time=s.curr_time;
+		System.out.println("broadcasting block "+ b.id +" from "+ this.id);
 		for (int i=0;i<peers.size();i++){
-			int id1=peers.get(i);
+			int id1=peers.get(i).id;
 			double latency=s.latency(this.id,id1,1);
+
 			Task task=new Task(1,curr_time+latency,id1);			//type,time,node
 			s.MainQueue.add(task);
 		}
--- Transaction.java
+++ Transaction.java
@@ -4,13 +4,15 @@ public class Transaction{
   int toID;
   double amount;
   boolean spent;
+  double time;
   //how to generate transaction ID?
-  Transaction(int tID, int fromID, int toID, double amount){
+  Transaction(int tID, int fromID, int toID, double amount,double t){
     this.tID = tID;
     this.fromID = fromID;
     this.toID = toID;
     this.amount = amount;
     this.spent=false;
+    this.time=t;
   }
 
   Transaction(Transaction tr){
@@ -19,5 +21,13 @@ public class Transaction{
     toID = tr.toID;
     amount = tr.amount;
     this.spent=false;
+    this.time=tr.time;
+  }
+  public int hashCode(){
+  	return this.tID;
+  }
+
+  public boolean equals(Transaction t){
+  	return (this.tID==t.tID);
   }
 }
