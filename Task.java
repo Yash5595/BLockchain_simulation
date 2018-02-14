c4c13df3d78747e8bd4beda2c65ebe0b9af679b1
Yash5595 <y.tiwari56@gmail.com>
2018-02-14 14:15:50 +0530

    Task

--- Task.java
+++ Task.java
@@ -1,3 +1,21 @@
 public class Task{
-	
+	//Types of tasks
+	static final int GENERATE_BLOCK=0;
+	static final int RECEIVE_BLOCK=1;
+	static final int GENERATE_TRANS=2;
+	static final int RECEIVE_TRANS=3;
+
+	int scTime;
+	int type;
+
+
+	Task(int type,int scTime,Node node,Simulator simulator,int id){
+		switch(type){
+			case GENERATE_BLOCK:
+				break;
+			case RECEIVE_BLOCK:
+				node.
+		}
+	}
+
 }
