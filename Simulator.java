89985ea94042ef083427ea43e6e76ac3b1b1e172
Yash5595 <y.tiwari56@gmail.com>
2018-02-14 14:17:19 +0530

    Simulator

--- Simulator.java
+++ Simulator.java
@@ -1,39 +1,59 @@
+import java.util.*;
+public class taskComparator implements comparator<Task>{
+	@Override
+	pubilc int compare(task t1,task t2){
+		
+	}
+}
+
 public class Simulator{
+	//inputs
+	static int n;
+	static int z;
 
+
+	//Parameters
+	double LAMBDA_TRANS;
+	double D_IJ_MEAN;
+	double LAMBDA_TK;
+	double[][] P_IJ=new double[n][n];
+
+	//priority queue for scheduling tasks
+	PriorityQueue<Task> MainQueue;
 	ArrayList<Node> nodes;
-	ArrayList<ArrayList<Block>> blocks;
-	ArrayList<ArrayList<Transaction>> trans;
-	Random rand;
+	Random random;
 	double curr_time;
-
+	
 	Simulator(){
 
 		nodes = new ArrayList<Node>();
-		blocks = new ArrayList<ArrayList<Block>>(n);
-		trans = new ArrayList<ArrayList<Transaction>>(n);
-		rand = new Random();
+		random = new Random();
 		curr_time = 0.0;
+		Float random_float;
 
 		for(int i=0;i<n;i++){
-			blocks.add(i,ArrayList<Block>());
-			trans.add(i,ArrayList<Transaction>());
+			for(int j=0;j<n;j++){
+				random_float=random.nextFloat();
+				P_IJ[i][j]=10.0+random_float*490.0;
+			}
 		}
-
+	
 		
 	}
 
 	void init(){
-
-		double mean = 2.0;   								///////// take input
-
+		Node new_node;
 		for(int i=0;i<n;i++){
-			int rand2 = rand.nextInt(98) +1;
-				Node new_node = new Node(i,true,mean,)
+			Float random_float=random.nextFloat()*100;
+			if(random_float<z){
+				new_node=new Node(i,false,LAMBDA_TRANS,n);
+			}else{
+				new_node=new Node(i,true,LAMBDA_TRANS,n);
 			}
+			nodes.add(new_node);
 		}
 
 	}
-
 
 	double latency(int i, int j,int size){
 
@@ -41,11 +61,11 @@ public class Simulator{
 		Node p_j = nodes.get(j);
 
 		double c_ij;
-		if(p_i.fast && p_j.fast){
-			c_ij = 100000;
+		if(p_i.fast && p_j.fast ==true){
+			c_ij = 100000000;
 		}
 		else{
-			c_ij = 5000;
+			c_ij = 5000000;
 		}
 
 		double b = (size*8.0*1000000)/c_ij; 							////// size= 0 when transaction & 1 when block
@@ -54,7 +74,6 @@ public class Simulator{
 		double laten = b + P_IJ[i][j] + d;
 		return laten;
 	}
-
 
 
 
